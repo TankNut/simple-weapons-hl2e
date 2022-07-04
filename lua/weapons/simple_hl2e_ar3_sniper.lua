09b5bf79ab79d27516c6c843dd8f70a44d09dc07
TankNut <m.vlasblom@live.nl>
2022-07-05 01:53:41 +0200

    AR3 sniper

---
+++ lua/weapons/simple_hl2e_ar3_sniper.lua
@@ -0,0 +1,69 @@
+AddCSLuaFile()
+
+SWEP.Base = "simple_base_scoped"
+
+SWEP.PrintName = "CIMR"
+SWEP.Category = "Simple Weapons: Half-Life 2 Extended"
+
+SWEP.Slot = 3
+
+SWEP.Spawnable = true
+
+SWEP.UseHands = true
+
+SWEP.ViewModelTargetFOV = 54
+
+SWEP.ViewModel = Model("models/simple_weapons/weapons/c_ar3_sniper.mdl")
+SWEP.WorldModel = Model("models/simple_weapons/weapons/w_ar3_sniper.mdl")
+
+SWEP.HoldType = "smg"
+SWEP.LowerHoldType = "passive"
+
+SWEP.Firemode = 0
+
+SWEP.Primary = {
+	Ammo = "AR2",
+
+	ClipSize = 20,
+	DefaultClip = 40,
+
+	Damage = 32,
+	Delay = 60 / 200,
+
+	Range = 3000,
+	Accuracy = 12,
+
+	RangeModifier = 0.98,
+
+	Recoil = {
+		MinAng = Angle(2, -0.6, 0),
+		MaxAng = Angle(2.4, 0.6, 0),
+		Punch = 0.4,
+		Ratio = 0.6
+	},
+
+	Sound = "Weapon_AR2.Single",
+	TracerName = "AirboatGunTracer"
+}
+
+SWEP.ViewOffset = Vector(0, 0, -1)
+
+SWEP.ScopeZoom = 4.5
+SWEP.ScopeSound = "NPC_CombineCamera.Click"
+
+function SWEP:ConsumeAmmo()
+	self:TakePrimaryAmmo(2)
+end
+
+function SWEP:DoImpactEffect(tr, dmgtype)
+	if not IsFirstTimePredicted() then
+		return
+	end
+
+	local effect = EffectData()
+
+	effect:SetOrigin(tr.HitPos + tr.HitNormal)
+	effect:SetNormal(tr.HitNormal)
+
+	util.Effect("AR2Impact", effect)
+end
